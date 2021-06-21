777fe35d0e8f9e126088f0f6f345b1d581f47b63
adisingh925 <adisingh925@gmail.com>
2021-06-21 12:14:46 +0530

    timetable app

--- app/src/main/java/com/example/navigation/monday_data.kt
+++ app/src/main/java/com/example/navigation/monday_data.kt
@@ -3,33 +3,22 @@ package com.example.navigation
 import android.app.*
 import android.content.DialogInterface
 import android.content.Intent
-import android.graphics.BitmapFactory
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.os.storage.StorageManager
-import android.provider.MediaStore
 import android.provider.Settings
 import android.widget.ImageButton
-import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.auth.api.signin.internal.Storage
+import androidx.core.view.isVisible
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.ktx.storage
 import com.karumi.dexter.Dexter.*
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionDeniedResponse
 import com.karumi.dexter.listener.PermissionGrantedResponse
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.single.PermissionListener
-import io.grpc.Context
-import java.io.File
-import java.io.InputStream
-import java.util.jar.Manifest
 
 
 class monday_data : AppCompatActivity() {
@@ -38,6 +27,8 @@ class monday_data : AppCompatActivity() {
 
     lateinit var uri:Uri
 
+    lateinit var progbar:ProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_monday_data)
@@ -45,6 +36,10 @@ class monday_data : AppCompatActivity() {
         val back = findViewById<ImageButton>(R.id.imageview)
 
         val upload = findViewById<FloatingActionButton>(R.id.floatingActionButton2)
+
+        progbar = findViewById(R.id.progressBar)
+
+        progbar.isVisible = false
 
         back.setOnClickListener()
         {
@@ -110,15 +105,18 @@ class monday_data : AppCompatActivity() {
             }
 
             var riversRef = storageref.child("uploads/"+System.currentTimeMillis()+".pdf")
+            progbar.isVisible = true
 
             riversRef.putFile(uri)
                 .addOnSuccessListener()
             {
-                Toast.makeText(this,"file upload success",Toast.LENGTH_SHORT)
+                Toast.makeText(this,"file upload success",Toast.LENGTH_SHORT).show()
+                progbar.isVisible = false
             }
                 .addOnFailureListener()
                 {
-                    Toast.makeText(this,"file upload failed",Toast.LENGTH_SHORT)
+                    Toast.makeText(this,"file upload failed",Toast.LENGTH_SHORT).show()
+                    progbar.isVisible = false
                 }
         }
     }
